c41a23c2839f69e022f640e16ff08b085b02a28c
Truong Long Khanh <85059988+khanhlong247@users.noreply.github.com>
2025-09-19 04:36:27 +0700

    Update urls.py

--- foodshopeight_be/api_gateway/urls.py
+++ foodshopeight_be/api_gateway/urls.py
@@ -1,9 +1,24 @@
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from rest_framework.routers import DefaultRouter
+from app_hr.views import StaffProfileViewSet
+from app_inventory.views import SupplierViewSet, IngredientViewSet, InventoryLotViewSet
+from app_menu.views import MenuItemViewSet, RecipeItemViewSet
 
 router = DefaultRouter()
 app_name = 'api_gateway'
+
+# ---- HR ----
+router.register(r"hr/staff-profiles", StaffProfileViewSet, basename="hr-staff-profiles")
+
+# ---- INVENTORY ----
+router.register(r"inventory/suppliers", SupplierViewSet, basename="inventory-suppliers")
+router.register(r"inventory/ingredients", IngredientViewSet, basename="inventory-ingredients")
+router.register(r"inventory/lots", InventoryLotViewSet, basename="inventory-lots")
+
+# ---- MENU ----
+router.register(r"menu/items", MenuItemViewSet, basename="menu-items")
+router.register(r"menu/recipes", RecipeItemViewSet, basename="menu-recipes")
 
 urlpatterns = [
     path('', include(router.urls)),  # Include the router URLs
